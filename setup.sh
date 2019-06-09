109b6372930493b883e07a3abac276e308b31921
kimotu4632uz <kimotu.phone@gmail.com>
2019-06-09 13:23:52 +0900

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ dotfile=[
 ]
 
 source=[
+    "utils.sh",
     "git-prompt.sh.src",
     "bashmarks.sh.src"
 ]
@@ -13,17 +14,22 @@ bin=[
     "24-bit-color.sh.src"
 ]
 
+export MYENV=${MYENV:-$HOME/.myenv}
+git clone https://github.com/kimotu4632uz/myenv.git $MYENV
+cd $MYEMV
+
 for file in ${dotfile[@]}; do
     ln -s src/$file $HOME/
 done
 
 for t in "source" "bin"; do
     for file in ${$t[@]}; do
+        [[ $t ]] || mkdir $t
         if [[ ${file#*.} == "src" ]]; then
-            bash $file
+            bash $t/$file
             mv src/${file%.*} $t/
         else
-            ln -s src/$file $t
+            ln -s src/$file $t/
         fi
     done
 done
