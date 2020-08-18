cbf41921aec540dee6b2a922b4e06d0e32d1dce2
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2020-08-18 17:25:01 +0900

    update pass_gen.sh

--- bin/pass_gen.sh
+++ bin/pass_gen.sh
@@ -1,26 +1,29 @@
-#!/bin/bash -e
+#!/bin/bash -eu
+
+let al=0 num=0 length=10
+set=""
 
 while getopts 'anl:s:h' OPT; do
   case $OPT in
-    a) alonly=1 ;;
-    n) numonly=1 ;;
-    l) length=$OPTARG ;;
+    a) al=1 ;;
+    n) num=1 ;;
+    l) let length=$OPTARG ;;
     s) set=$OPTARG ;;
     h) echo "Usage: pass_gen [-a] [-n] [-s string_set] [-l length]"; exit 0 ;;
     *) exit 1 ;;
   esac
 done
 
-length="${length:-10}"
-
-if ((alonly && numonly)); then
-  cat /dev/urandom | tr -dc '[:alnum:]' | fold -w $length | head -1
-elif (( alonly )); then
-  cat /dev/urandom | tr -dc '[:alpha:]' | fold -w $length | head -1
-elif (( numonly )); then
-  cat /dev/urandom | tr -dc '[:digit:]' | fold -w $length | head -1
-elif [ "$set" != "" ]; then
-  cat /dev/urandom | tr -dc "$set" | fold -w $length | head -1
-else
-  cat /dev/urandom | tr -dc '[:graph:]' | fold -w $length | head -1
+if [[ "$set" == "" ]]; then
+  if ((al && num)); then
+    set='[:alnum:]'
+  elif (( al )); then
+    set='[:alpha:]'
+  elif (( num )); then
+    set='[:digit:]'
+  else
+    set='[:graph:]'
+  fi
 fi
+
+cat /dev/urandom | tr -dc "$set" | fold -w $length | head -1
