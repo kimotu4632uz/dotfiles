1edfdc7c68fc3b126891a5130b2bfd139167f7f2
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2021-05-03 13:01:15 +0900

    modify encode.sh and remove tag related scritps

--- bin/encode.sh
+++ bin/encode.sh
@@ -1,74 +1,75 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
+set -o pipefail
 
-source $MYENV/source/utils.sh
+cleanup() {
+  cd "$HOME"
+  rm -rf "$tmpdir"
+}
 
-[[ $# == 0 ]] && { echo "Usage: encode.sh [--picture PICTURE] JSON"; exit 0; }
-            
+err_handle() {
+  local status=$?
+  echo "[$BASH_SOURCE:$LINENO] - "$BASH_COMMAND" returns status $status"
+  cleanup
+  exit $status
+}
 
-for OPT in "$@"; do
-    case $OPT in
-        -h | --help)
-            echo "Usage: encode.sh [--picture PICTURE] JSON"
-            exit 0
-            ;;
-
-        --picture)
-            picture="$2"
-            shift 2
-            ;;
-
-        -*)
-            echo "encode.sh: illegal option -- '$(echo $1 | sed 's/^-*//')'" 1>&2
-            exit 1
-            ;;
-
-        *)
-            if [[ ! -z "$1" ]] && [[ ! "$1" =~ ^-+ ]]; then
-                json="$1"
-                shift 1
-            fi
-            ;;
-    esac
-done
-
-
-echo "reading metadata from $json ..."
-
-declare -A meta=$(json2bash "$json")
-readarray -t tracks < <(jq -r '.tracks[].title' "$json")
-
-if [[ "${meta['artist']}" == "" ]]; then
-  readarray -t artists < <(jq -r '.tracks[].artist' "$json")
-else
-  declare -a artists=$(printf "\"${meta['artist']}\" ""%.s" ${!tracks[@]} | sed -E 's/^(.*)$/( \1 )/g')
-fi
-
-if [[ ${picture-''} == '' ]]; then
-    pic_opt=""
-else
-    pic_opt="--picture $PWD/$picture"
-fi
-
-tmpdir=$(mktemp -d)
-cd "$tmpdir"
-
-"$USERPROFILE"/Programs/cd-paranoia.exe -B
-
-id=$("$USERPROFILE"/Programs/cdio-discid.exe | tr -dc '[:alnum:]')
-
-if [[ $? != 0 ]]; then
+sig_handle() {
+  echo "Aborting..."
+  cleanup
   exit 1
-fi
+}
 
-for i in ${!tracks[@]}; do
-  iz=$(printf "%02d" $((i + 1)))
+print_help() {
+  echo "Usage: encode.sh [OPTIONS]"
+  echo ""
+  echo "OPTIONS:"
+  echo "  -o, --output <OUTDIR>  Place files to OUTDIR"
+}
 
-  flac -f -8 $pic_opt -T title="${tracks[i]}" -T artist="${artists[i]}" -T album="${meta['album']}" -T date="${meta['date']}" -T genre="${meta['genre']}" -T tracknumber=$((i + 1)) -o ${id}_${iz}.flac track${iz}.cdda.wav
-  #opusenc --bitrate 160 $pic_opt --title "${tracks[i]}" --artist "${artists[i]}" --album "$album" --date "$date" --genre "$genre" --tracknumber $((i + 1)) audio_${iz}.wav ${id}_${iz}.opus
-done
+main() {
+  local outdir="$USERPROFILE"/Music
 
-cd $HOME
-mv $tmpdir/*.flac "$USERPROFILE"/Music/
-#mv $tmpdir/*.opus "$USERPROFILE"/Music/Music
-rm -rf $tmpdir
+  for OPT in "$@"; do
+    case $OPT in
+      -h | --help)
+        print_help
+        exit 0
+        ;;
 
+      -o | --output)
+        outdir="${2%/}"
+        shift 2
+        ;;
+
+      -*)
+        echo "encode.sh: illegal option -- '$(echo $1 | sed 's/^-*//')'" 1>&2
+        exit 1
+        ;;
+    esac
+  done
+
+  [[ "$USERPROFILE" == "" ]] && { echo "Error: env \$USERPROFILE is not set"; exit 1; }
+
+  trap sig_handle SIGINT
+  trap err_handle ERR
+
+  local tmpdir id count
+
+  tmpdir=$(mktemp -d)
+  cd "$tmpdir"
+
+  "$USERPROFILE"/Programs/cd-paranoia.exe -B
+
+  id=$("$USERPROFILE"/Programs/cdio-discid.exe | tr -dc '[:alnum:]')
+  count=$(find . -maxdepth 1 -mindepth 1 -name "*.wav" | wc -l)
+
+  for iz in $(seq -f "%02g" "$count"); do
+    flac -f -8 -o "$outdir"/"${id}_${iz}.flac" "track${iz}.cdda.wav"
+    #opusenc --bitrate 160 "track${iz}.cdda.wav" "$outdir"/"${id}_${iz}.opus"
+  done
+
+  cleanup
+}
+
+main "$@"
