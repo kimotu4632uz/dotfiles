8112150dd23d4f7a26ba47acc3756bc419c893f8
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2021-11-06 17:37:23 +0900

    add support for wsl

--- setup.sh
+++ setup.sh
@@ -1,30 +1,58 @@
 #!/bin/bash
-export MYENV=${MYENV:-$HOME/.myenv}
-git clone git@github.com:kimotu4632uz/myenv.git $MYENV
 
-while read file; do
-  ln -sf "$file" $HOME/"${file##*/}"
-done < <(find $MYENV/dotfiles -maxdepth 1 -type f)
+check_wsl() {
+  type cmd.exe &> /dev/null
+}
 
-if [ ! -d $MYENV/bin/3rdparty ]; then
-  mkdir -p $MYENV/bin/3rdparty 
-fi
+init() {
+  export MYENV=${MYENV:-$HOME/.myenv}
 
-if [ ! -d $MYENV/source/3rdparty ]; then
-  mkdir -p $MYENV/source/3rdparty 
-fi
-
-while read file; do
-  (
-  source "$file"
-  curl -o "${file%.*}" "$url"
-
-  if [[ "$patch" != "" ]]; then
-    patch --backup-if-mismatch -u "${file%.*}" < "${file%/*}/$patch"
+  if [ ! -d $MYENV ]; then
+    git clone git@github.com:kimotu4632uz/myenv.git $MYENV
   fi
 
-  mv "${file%.*}" $(echo "$file" | sed -E 's/^(.*)\/(.*)\.(.*)$/\1\/3rdparty\/\2/')
-  )
-done < <(find $MYENV/{source,bin} -maxdepth 1 -type f -name "*.src")
+  mkdir -p $MYENV/{bin,source}/3rdparty 
+}
 
-find $MYENV/bin -type f -not -name "*.src" -exec chmod +x {} \;
+install_dotfiles() {
+  while read file; do
+    ln -sf "$file" $HOME/"${file##*/}"
+  done
+}
+
+install_bins() {
+  while read file; do
+    (
+    source "$file"
+    curl -o "${file%.*}" "$url"
+  
+    if [[ "$patch" != "" ]]; then
+      patch --backup-if-mismatch -u "${file%.*}" < "${file%/*}/$patch"
+    fi
+  
+    mv "${file%.*}" $(echo "$file" | sed -E 's/^((.*)\/wsl|(.*))\/(.*)\.(.*)$/\2\3\/3rdparty\/\4/')
+    )
+  done
+}
+
+main() {
+  init
+
+  find $MYENV/dotfiles -maxdepth 1 -type f | install_dotfiles
+  if check_wsl && [ -d $MYENV/dotfiles/wsl ]; then
+    find $MYENV/dotfiles/wsl -maxdepth 1 -type f | install_dotfiles
+  fi
+
+  find $MYENV/{bin,source} -maxdepth 1 -type f -name "*.src" | install_bins
+  if check_wsl && [ -d $MYENV/bin/wsl ]; then
+    find $MYENV/bin/wsl -maxdepth 1 -type f -name "*.src" | install_bins
+  fi
+  if check_wsl && [ -d $MYENV/source/wsl ]; then
+    find $MYENV/source/wsl -maxdepth 1 -type f -name "*.src" | install_bins
+  fi
+
+  find $MYENV/bin -type f -not -name "*.src" -exec chmod +x {} \;
+}
+
+main
+
