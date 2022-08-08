51d4da8e5b20c73c183fac099f0bc3ede5cbde50
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2022-08-08 21:52:45 +0900

    update to module style

--- fish/config/fish/config.fish
+++ fish/config/fish/config.fish
@@ -1,4 +1,3 @@
 if status is-interactive
-  source ~/.myenv/env
   starship init fish | source
 end
--- setup.sh
+++ setup.sh
@@ -1,83 +1,77 @@
 #!/bin/bash -e
 
-check_wsl() {
-  type cmd.exe &> /dev/null
-}
-
 print_help() {
-  echo "usage: $0 [-s SHELL]"
+  echo "usage: $0 [MODULES]"
   exit
 }
 
-init() {
-  if [ ! -d $MYENV ]; then
-    git clone https://github.com/kimotu4632uz/myenv.git $MYENV
-  fi
-
-  mkdir -p $MYENV/bin/3rdparty 
-}
-
-install_shellrc() {
-  local shell=$1
-
-  cd "$MYENV/shell/$shell"
-
-  for line in $(find . -mindepth 1); do
-    if [ -f "$line" ]; then
-      ln -sf "$MYENV/shell/$shell/${line#*/}" $HOME/"${line#*/}"
-    else
-      mkdir -p $HOME/"${line#*/}"
-    fi
-  done
-}
-
-install_dotfiles() {
-  local dir="$1"
-
-  for file in $(find "$dir" -maxdepth 1 -type f); do
-    ln -sf "$file" $HOME/"${file##*/}"
-  done
-}
-
-install_config() {
-  cd $MYENV/config
-  mkdir -p $HOME/.config
-
-  for line in $(find . -mindepth 1); do
-    local src="$MYENV/config/${line#*/}" 
-    local dst="$HOME/.config/${line#*/}"
-
-    if [ -f "$src" ]; then
-      ln -sf "$src" "$dst"
-    else
-      mkdir -p "$dst"
-    fi
-  done
-}
 
 install_bin() {
   local dir="$1"
+  local dst="$HOME/.local/bin"
+
+  mkdir -p "$dst"
 
   for file in $(find "$dir" -maxdepth 1 -type f -name "*.src"); do
     (
     source "$file"
     curl -o "${file%.*}" "$url"
+    chmod +x "${file%.*}"
   
     if [[ "$patch" != "" ]]; then
       patch --backup-if-mismatch -u "${file%.*}" < "${file%/*}/$patch"
     fi
   
-    mv "${file%.*}" $(echo "$file" | sed -E 's/^((.*)\/wsl|(.*))\/(.*)\.(.*)$/\2\3\/3rdparty\/\4/')
+    mv "${file%.*}" "$dst/"
     )
+  done
+
+  for file in $(find "$dir" -maxdepth 1 -type f -not -name "*.src"); do
+    ln -sf "$PWD/$dir/${file##*/}" "$dst/${file##*/}"
   done
 }
 
+
+install_module() {
+  local module="$1"
+
+  echo "install module ${module}..."
+
+  for line in $(cd $module && find . -mindepth 1 -maxdepth 1 -type f); do
+    local file=${line#*/}
+    local src="$module/$file" 
+    local dst="$HOME/.$file"
+
+    echo "$src -> $dst"
+    ln -sf "$PWD/$src" "$dst"
+  done
+
+  if [[ -d "$module/config" ]]; then
+    for line in $(cd "$module/config" && find . -mindepth 1); do
+      local entry="${line#*/}"
+      local src="$module/config/$entry"
+      local dst="$HOME/.config/$entry"
+
+      if [[ -f "$src" ]]; then
+        echo "$src -> $dst"
+        ln -sf "$PWD/$src" "$dst"
+      else
+        mkdir -p "$dst"
+      fi
+    done
+  fi
+ 
+  if [[ -d "$module/bin" ]]; then
+    echo "install binary..."
+    install_bin "$module/bin"
+  fi
+
+  echo ""
+}
+
 main() {
-  local shellrc="bash"
-  while getopts s:h OPT; do
+  while getopts h OPT; do
     case $OPT in
-      s) shellrc=$OPTARG
-         ;;
       h) print_help
          ;;
       \?) print_help
@@ -85,21 +79,12 @@ main() {
     esac
   done
 
-  init
+  modules=("$@")
 
-  install_shellrc $shellrc
-  install_dotfiles "$MYENV/dotfiles"
-  install_config "$MYENV/config"
-  install_bin "$MYENV/bin"
-
-  if check_wsl; then
-    [ -d $MYENV/dotfiles/wsl ] && install_dotfiles "$MYENV/dotfiles/wsl"
-    [ -d $MYENV/bin/wsl ] && install_bin "$MYENV/bin/wsl"
-  fi
-
-  find $MYENV/bin -type f -not -name "*.src" -exec chmod +x {} \;
+  for module in "${modules[@]}"; do
+    install_module "$module"
+  done
 }
 
-export MYENV=${MYENV:-$HOME/.myenv}
 main "$@"
 
