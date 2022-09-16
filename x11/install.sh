6a0a69db28abf7d7ed8efaa3630bd79008fdb0b8
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2022-09-16 16:25:00 +0900

    x11: improve install.sh

--- x11/install.sh
+++ x11/install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd ${0%/*}; pwd)"
+
 if [[ "$1" == "i3" ]]; then
-  ln -s Xmodmap ~/.Xmodmap
-  sudo cp 10-touchpad.conf /etc/X11/xorg.conf.d/
+  ln -s "$SCRIPT_DIR/x11/Xmodmap" "$HOME/.Xmodmap"
+  sudo cp "$SCRIPT_DIR/x11/10-touchpad.conf" /etc/X11/xorg.conf.d/
 
 elif [[ "$1" == "sway" ]]; then
-  ln -s jp106_custom ~/.xkb/symbols/jp106_custom
+  ln -s "$SCRIPT_DIR/x11/jp106_custom" "$HOME/.xkb/symbols/jp106_custom"
 fi
