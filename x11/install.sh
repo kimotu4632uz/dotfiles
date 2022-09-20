7eef17bdc64223da832838caa359d22fdc5ffb74
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2022-09-20 11:39:57 +0900

    x11: fix typo

--- x11/install.sh
+++ x11/install.sh
@@ -3,9 +3,9 @@
 SCRIPT_DIR="$(cd ${0%/*}; pwd)"
 
 if [[ "$1" == "i3" ]]; then
-  ln -s "$SCRIPT_DIR/x11/Xmodmap" "$HOME/.Xmodmap"
-  sudo cp "$SCRIPT_DIR/x11/10-touchpad.conf" /etc/X11/xorg.conf.d/
+  ln -s "$SCRIPT_DIR/Xmodmap" "$HOME/.Xmodmap"
+  sudo cp "$SCRIPT_DIR/10-touchpad.conf" /etc/X11/xorg.conf.d/
 
 elif [[ "$1" == "sway" ]]; then
-  ln -s "$SCRIPT_DIR/x11/jp106_custom" "$HOME/.xkb/symbols/jp106_custom"
+  ln -s "$SCRIPT_DIR/jp106_custom" "$HOME/.xkb/symbols/jp106_custom"
 fi
