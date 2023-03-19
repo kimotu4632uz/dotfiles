d9d334a5b22ad9d7a1edbf56787a7b42f7565061
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2023-03-19 15:31:45 +0900

    xkeysnail: update config

--- xkeysnail/config/xkeysnail/config.py
+++ xkeysnail/config/xkeysnail/config.py
@@ -4,13 +4,19 @@ import re
 from xkeysnail.transform import *
 
 # [Global modemap] Change modifier keys as in xmodmap
-define_modmap({
-    Key.CAPSLOCK: Key.LEFT_CTRL
-})
+#define_modmap({
+#    Key.CAPSLOCK: Key.LEFT_CTRL
+#})
 
-# SandS
+# [Multipurpose modmap] Give a key two meanings. A normal key when pressed and
+# released, and a modifier key when held down with another key. See Xcape,
+# Carabiner and caps2esc for ideas and concept.
 define_multipurpose_modmap({
+    # SandS
     Key.SPACE: [Key.SPACE, Key.LEFT_SHIFT],
+    # ALT as Henkan, Muhenkan
+    Key.LEFT_ALT: [Key.MUHENKAN, Key.LEFT_ALT],
+    Key.RIGHT_ALT: [Key.HENKAN, Key.RIGHT_ALT]
 })
 
 # Send muhenkan when enter Esc
@@ -21,3 +27,4 @@ define_keymap(re.compile('Alacritty'), {
 define_keymap(re.compile('obsidian'), {
     K('esc'): [K('muhenkan'), K('esc')]
 }, "Esc and IME off")
+
