71e4d5532dcde35bbbc69de8c3d84d139938d96c
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2023-04-07 13:20:43 +0900

    laptop: add libinput-gestures register script

--- laptop/autorun.sh
+++ laptop/autorun.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 sudo gpasswd -a $USER input
+libinput-gestures-setup
+libinput-gestures-setup autostart
+libinput-gestures-setup start
 
 sudo systemctl enable tlp
 sudo systemctl start tlp
+
